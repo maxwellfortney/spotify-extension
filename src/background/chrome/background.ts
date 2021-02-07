f6566a2a3bd84777756cfa8d1714b7056d1214cf
davidnguyen179 <dzungnguyen179@gmail.com>
2021-02-07 14:14:17 +0900

    Upgrade Chrome manifest from v2 to v3

--- src/background/chrome/background.ts
+++ src/background/chrome/background.ts
@@ -1,5 +1,5 @@
 /// <reference types="chrome"/>
-import { CONTEXT_MENU_ITEM, WEB_PLAYER_URL, HOUR_IN_SECOND } from '../../lib/constants';
+import { CONTEXT_MENU_ITEM, WEB_PLAYER_URL, HOUR_IN_SECOND, CONTEXT_MENU_ITEM_TEXT } from '../../lib/constants';
 import { isChristmasPeriod } from '../../lib/utils';
 
 setChristmasIcon();
@@ -19,7 +19,7 @@ chrome.runtime.onInstalled.addListener(function () {
   // Create right click menu
   chrome.contextMenus.create({
     id: CONTEXT_MENU_ITEM,
-    title: 'Search Spotify for "%s"',
+    title: CONTEXT_MENU_ITEM_TEXT,
     contexts: ['selection'],
   });
 });
@@ -32,10 +32,13 @@ chrome.contextMenus.onClicked.addListener(function (info) {
   }
 });
 
+// TODO: Add type support for "chrome.action"
 function setChristmasIcon() {
   if (isChristmasPeriod()) {
-    chrome.browserAction.setIcon({ path: 'images/spotify-mini-player-xmas-128.png' });
+    // @ts-ignore
+    chrome.action.setIcon({ path: 'images/spotify-mini-player-xmas-128.png' });
   } else {
-    chrome.browserAction.setIcon({ path: 'images/spotify-mini-player-128.png' });
+    // @ts-ignore
+    chrome.action.setIcon({ path: 'images/spotify-mini-player-128.png' });
   }
 }
--- src/background/edge/background.ts
+++ src/background/edge/background.ts
@@ -1,5 +1,5 @@
 /// <reference types="chrome"/>
-import { CONTEXT_MENU_ITEM, WEB_PLAYER_URL, HOUR_IN_SECOND } from '../../lib/constants';
+import { CONTEXT_MENU_ITEM, WEB_PLAYER_URL, HOUR_IN_SECOND, CONTEXT_MENU_ITEM_TEXT } from '../../lib/constants';
 import { isChristmasPeriod } from '../../lib/utils';
 
 setChristmasIcon();
@@ -19,7 +19,7 @@ chrome.runtime.onInstalled.addListener(function () {
   // Create right click menu
   chrome.contextMenus.create({
     id: CONTEXT_MENU_ITEM,
-    title: 'Search Spotify for "%s"',
+    title: CONTEXT_MENU_ITEM_TEXT,
     contexts: ['selection'],
   });
 });
